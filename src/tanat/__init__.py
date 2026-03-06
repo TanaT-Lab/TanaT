fa7e55fa7ebb32d5cdfac925b5f73ce0aa72825f
Arnaud Duvermy <arnaud.duvermy@inria.fr>
2026-03-06 18:58:01 +0100

    refactor: reorganize and enhance workspace utility functions and internal builder registry

--- src/tanat/__init__.py
+++ src/tanat/__init__.py
@@ -2,3 +2,45 @@
 """
 TanaT - Temporal Analysis of Trajectories
 """
+
+import logging
+from pathlib import Path
+
+from .core import registry as _registry
+from .core import context
+from .store.factory import StoreFactory
+from .core.workspace import Workspace
+from .sequence.base.pool import SequencePool
+from .trajectory.pool import TrajectoryPool
+
+logging.getLogger("tanat").addHandler(logging.NullHandler())
+LOGGER = logging.getLogger(__name__)
+
+
+# Workspace utility functions
+def set_workspace(path: str):
+    """
+    Set the active workspace to a new directory.
+    """
+    new_ws = Workspace(path)
+    context.set_active_ws_instance(new_ws)
+    LOGGER.info("Workspace updated to: %s", new_ws.root)
+
+
+def get_workspace():
+    """
+    Get the active workspace instance.
+    """
+    return context.get_workspace()
+
+
+##  --- INTERNAL BUILDER REGISTRY ---
+def _sequence_pool_builder(path: Path) -> SequencePool:
+    store = StoreFactory.from_path(path)
+    sequence_type = store.get_sequence_type()
+    seqpool_cls = SequencePool.get_registered(sequence_type)
+    return seqpool_cls(store=store)
+
+
+_registry.register_factory("sequence", _sequence_pool_builder)
+_registry.register_factory("trajectory", lambda path: TrajectoryPool(store=path))
