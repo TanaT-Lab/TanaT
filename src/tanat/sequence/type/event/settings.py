1869346fb142c9b064849d258869c4cfdc737e52
Arnaud Duvermy <arnaud.duvermy@inria.fr>
2026-03-06 19:06:46 +0100

    feat: add EventSequenceSettings class for configuring event sequence settings

---
+++ src/tanat/sequence/type/event/settings.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""Event sequence settings."""
+
+from __future__ import annotations
+
+from tanat_utils import settings_dataclass as dataclass
+
+from ...base.settings import SequenceSettings
+
+
+@dataclass
+class EventSequenceSettings(SequenceSettings):
+    """Settings for event sequences (single timestamp column)."""
+
+    id_column: str
+    time_column: str
+
+    def get_temporal_columns(self) -> list[str]:
+        """Returns temporal columns for Event sequences [time]."""
+        return [self.time_column]
