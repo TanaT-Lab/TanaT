d35af998b271ce3cef05858da013877442a470ba
Arnaud Duvermy <arnaud.duvermy@inria.fr>
2026-03-16 11:38:27 +0100

    fix: remove UnsupportedSequenceTypeError from Spanplot package exports

--- src/tanat/visualization/sequence/type/spanplot/__init__.py
+++ src/tanat/visualization/sequence/type/spanplot/__init__.py
@@ -2,12 +2,10 @@
 """Spanplot package."""
 
 from .builder import SpanplotVizBuilder
-from .exception import UnsupportedSequenceTypeError
 from .settings import SpanAesthetics, SpanMarkerSettings, SpanplotSettings
 
 __all__ = [
     "SpanplotVizBuilder",
-    "UnsupportedSequenceTypeError",
     "SpanAesthetics",
     "SpanMarkerSettings",
     "SpanplotSettings",
