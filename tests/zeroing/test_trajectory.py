9076b78b91b8c251e0ae0d29e5b0e7a4218a10c2
Arnaud Duvermy <arnaud.duvermy@inria.fr>
2026-03-20 13:08:59 +0100

    test: add placeholder for TrajectoryPool-level T0 tests

---
+++ tests/zeroing/test_trajectory.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""
+Tests: TrajectoryPool-level T0 tests.
+
+Placeholder: trajectory-level zeroing tests will be added here once the
+TrajectoryPool T0 API is finalised.
+"""
