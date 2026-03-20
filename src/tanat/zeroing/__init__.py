89ba40763c4e9ed9a243ac0aa4994fb8454a45e8
Arnaud Duvermy <arnaud.duvermy@inria.fr>
2026-03-20 08:37:52 +0100

    feat: initialize zeroing package with T0 components

---
+++ src/tanat/zeroing/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Zeroing (T0) package.
+"""
+
+from .base import T0Setter, T0Value, _T0, _T0_NEAREST_RANK
+
+__all__ = [
+    "T0Setter",
+    "T0Value",
+    "_T0",
+    "_T0_NEAREST_RANK",
+]
