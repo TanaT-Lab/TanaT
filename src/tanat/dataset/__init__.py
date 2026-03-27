aef3cc73b57e1df2ff1a903a3a902e0df0fd5a4f
Arnaud Duvermy <arnaud.duvermy@inria.fr>
2026-03-27 17:44:07 +0100

    feat: update dataset package to include simulation sub-package for synthetic data generation

--- src/tanat/dataset/__init__.py
+++ src/tanat/dataset/__init__.py
@@ -2,5 +2,17 @@
 """Dataset package."""
 
 from .access.utils import access
+from .simulation import (
+    simulate_events,
+    simulate_intervals,
+    simulate_states,
+    simulate_trajectories,
+)
 
-__all__ = ["access"]
+__all__ = [
+    "access",
+    "simulate_events",
+    "simulate_intervals",
+    "simulate_states",
+    "simulate_trajectories",
+]
---
+++ src/tanat/dataset/simulation/__init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Simulation sub-package: synthetic data generation."""
+
+from .events import simulate_events
+from .intervals import simulate_intervals
+from .states import simulate_states
+from .trajectories import simulate_trajectories
+
+__all__ = [
+    "simulate_events",
+    "simulate_intervals",
+    "simulate_states",
+    "simulate_trajectories",
+]
