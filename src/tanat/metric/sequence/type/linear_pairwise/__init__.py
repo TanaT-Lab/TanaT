2ba2c95acc2667772fe03caf3ff36240113b0ac4
Arnaud Duvermy <arnaud.duvermy@inria.fr>
2026-04-10 16:38:05 +0200

    feat: add __init__.py for LinearPairwiseSequenceMetric package

---
+++ src/tanat/metric/sequence/type/linear_pairwise/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""
+LinearPairwiseSequenceMetric package.
+"""
+
+from .metric import LinearPairwiseSequenceMetric, LinearPairwiseSettings
+
+__all__ = ["LinearPairwiseSequenceMetric", "LinearPairwiseSettings"]
