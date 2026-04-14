6bea0fa15af12ef592690bd391e42d24616cef97
Arnaud Duvermy <arnaud.duvermy@inria.fr>
2026-04-14 16:30:39 +0200

    feat: add __init__.py for aggregation trajectory metric sub-package

---
+++ src/tanat/metric/trajectory/type/aggregation/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Aggregation trajectory metric sub-package.
+"""
+
+from .metric import AggregationTrajectoryMetric, AggregationSettings
+
+__all__ = [
+    "AggregationTrajectoryMetric",
+    "AggregationSettings",
+]
