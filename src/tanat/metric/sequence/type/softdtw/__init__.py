2e220f62aa8fd40fc71c196a168d8c9e69f17656
Arnaud Duvermy <arnaud.duvermy@inria.fr>
2026-04-20 10:31:02 +0200

    feat: add __init__.py for SoftDTWSequenceMetric package

---
+++ src/tanat/metric/sequence/type/softdtw/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""
+SoftDTWSequenceMetric package.
+"""
+
+from .metric import SoftDTWSequenceMetric, SoftDTWSettings
+
+__all__ = ["SoftDTWSequenceMetric", "SoftDTWSettings"]
