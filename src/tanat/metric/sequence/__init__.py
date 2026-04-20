8953eb7a9fed2de6f428474615adaa6f61e85f28
Arnaud Duvermy <arnaud.duvermy@inria.fr>
2026-04-20 10:31:20 +0200

    feat: update __init__.py to include additional sequence metrics

--- src/tanat/metric/sequence/__init__.py
+++ src/tanat/metric/sequence/__init__.py
@@ -8,9 +8,27 @@ from .type.linear_pairwise.metric import (
     LinearPairwiseSequenceMetric,
     LinearPairwiseSettings,
 )
+from .type.edit.metric import EditSequenceMetric, EditSettings
+from .type.lcs.metric import LCSSequenceMetric, LCSSettings
+from .type.lcp.metric import LCPSequenceMetric, LCPSettings
+from .type.dtw.metric import DTWSequenceMetric, DTWSettings
+from .type.softdtw.metric import SoftDTWSequenceMetric, SoftDTWSettings
+from .type.chi2.metric import Chi2SequenceMetric, Chi2Settings
 
 __all__ = [
     "SequenceMetric",
     "LinearPairwiseSequenceMetric",
     "LinearPairwiseSettings",
+    "EditSequenceMetric",
+    "EditSettings",
+    "LCSSequenceMetric",
+    "LCSSettings",
+    "LCPSequenceMetric",
+    "LCPSettings",
+    "DTWSequenceMetric",
+    "DTWSettings",
+    "SoftDTWSequenceMetric",
+    "SoftDTWSettings",
+    "Chi2SequenceMetric",
+    "Chi2Settings",
 ]
