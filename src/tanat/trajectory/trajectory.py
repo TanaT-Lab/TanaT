8926963df1deec44438a12dd652d587213cfec00
Arnaud Duvermy <arnaud.duvermy@inria.fr>
2026-05-04 10:58:23 +0200

    refactor(trajectory): rename _apply_masks to _apply_id_mask for clarity

--- src/tanat/trajectory/trajectory.py
+++ src/tanat/trajectory/trajectory.py
@@ -284,7 +284,7 @@ class Trajectory(TrajectoryViewMixin, CachableSettings):
     # Masking helpers
     # ------------------------------------------------------------------
 
-    def _apply_masks(self, lf: pl.LazyFrame, **_) -> pl.LazyFrame:
+    def _apply_id_mask(self, lf: pl.LazyFrame, **_) -> pl.LazyFrame:
         """Scopes a LazyFrame to this trajectory's ID."""
         return lf.filter(pl.col(self._store.traj_id_col) == self._id_value)
 
