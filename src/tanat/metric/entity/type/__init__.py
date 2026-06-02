3d4f7d22ebced05ab14b54204c1f89a8c1ac40d8
Arnaud Duvermy <arnaud.duvermy@inria.fr>
2026-06-02 15:58:40 +0200

    refactor: update docstring and reorganize imports in entity metric type module

--- src/tanat/metric/entity/type/__init__.py
+++ src/tanat/metric/entity/type/__init__.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python3
-"""
-Entity metric type sub-package.
-"""
+"""Entity metric subtypes."""
+
+from .hamming.metric import HammingEntityMetric, HammingSettings
+
+__all__ = [
+    "HammingEntityMetric",
+    "HammingSettings",
+]
