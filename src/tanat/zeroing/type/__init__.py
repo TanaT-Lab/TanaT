c9637d4d2bb2e4e5073d51f84bd4368ce2a8c919
Arnaud Duvermy <arnaud.duvermy@inria.fr>
2026-06-02 15:59:46 +0200

    refactor: update package docstring and define T0Setter subtypes

--- src/tanat/zeroing/type/__init__.py
+++ src/tanat/zeroing/type/__init__.py
@@ -1,2 +1,18 @@
 #!/usr/bin/env python3
-"""Package stub."""
+"""T0Setter subtypes."""
+
+from .direct import DirectT0Setter, DirectT0Settings
+from .feature import FeatureT0Setter, FeatureT0Settings
+from .position import PositionT0Setter, PositionT0Settings
+from .query import QueryT0Setter, QueryT0Settings
+
+__all__ = [
+    "DirectT0Setter",
+    "DirectT0Settings",
+    "FeatureT0Setter",
+    "FeatureT0Settings",
+    "PositionT0Setter",
+    "PositionT0Settings",
+    "QueryT0Setter",
+    "QueryT0Settings",
+]
