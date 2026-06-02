29d403acec841e19c4e894ac126ed3f33ee2d8c0
Arnaud Duvermy <arnaud.duvermy@inria.fr>
2026-06-02 16:42:45 +0200

    refactor: update package docstring and define state sequence subtypes

--- src/tanat/sequence/type/state/__init__.py
+++ src/tanat/sequence/type/state/__init__.py
@@ -1,2 +1,14 @@
 #!/usr/bin/env python3
-"""Package stub."""
+"""State sequence subtypes."""
+
+from .entity import StateEntity
+from .pool import StateSequencePool
+from .sequence import StateSequence
+from .settings import StateSequenceSettings
+
+__all__ = [
+    "StateEntity",
+    "StateSequencePool",
+    "StateSequence",
+    "StateSequenceSettings",
+]
