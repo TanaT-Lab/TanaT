42147290347ca751fe62575821c73082fe35c314
Arnaud Duvermy <arnaud.duvermy@inria.fr>
2026-06-02 15:57:45 +0200

    feat: add internal import for subtype registration in dataset access module

--- src/tanat/dataset/access/__init__.py
+++ src/tanat/dataset/access/__init__.py
@@ -3,4 +3,7 @@
 
 from .utils import access
 
+# Internal import subtypes to trigger registration via __init_subclass__.
+from . import type as _register_subtypes  # pylint: disable=unused-import
+
 __all__ = ["access"]
