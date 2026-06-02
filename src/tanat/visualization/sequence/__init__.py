6494de4041b1fc1adb9219fc81479845954b6785
Arnaud Duvermy <arnaud.duvermy@inria.fr>
2026-06-02 15:59:26 +0200

    refactor: add internal import for subtype registration in sequence visualization module

--- src/tanat/visualization/sequence/__init__.py
+++ src/tanat/visualization/sequence/__init__.py
@@ -3,4 +3,7 @@
 
 from .core import SequenceVisualizer
 
+# Internal import subtypes to trigger registration via __init_subclass__.
+from . import type as _register_subtypes  # pylint: disable=unused-import
+
 __all__ = ["SequenceVisualizer"]
