a4bb012c98b58c9b4144fd5a267d39b6fa41a645
Arnaud Duvermy <arnaud.duvermy@inria.fr>
2026-06-02 15:59:40 +0200

    refactor: update package docstring and register T0Setter subtypes

--- src/tanat/zeroing/__init__.py
+++ src/tanat/zeroing/__init__.py
@@ -4,10 +4,20 @@ Zeroing (T0) package.
 """
 
 from .base import T0Setter, T0Value, _T0, _T0_NEAREST_RANK
+from .type import (
+    DirectT0Setter,
+    FeatureT0Setter,
+    PositionT0Setter,
+    QueryT0Setter,
+)
 
 __all__ = [
     "T0Setter",
     "T0Value",
     "_T0",
     "_T0_NEAREST_RANK",
+    "DirectT0Setter",
+    "FeatureT0Setter",
+    "PositionT0Setter",
+    "QueryT0Setter",
 ]
