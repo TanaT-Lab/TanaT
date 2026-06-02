d695a3c674de42348947a30fbe7bf6546f4f519d
Arnaud Duvermy <arnaud.duvermy@inria.fr>
2026-06-02 15:59:11 +0200

    refactor: update package docstring and reorganize imports in data source package

--- src/tanat/store/source/__init__.py
+++ src/tanat/store/source/__init__.py
@@ -1,2 +1,9 @@
 #!/usr/bin/env python3
-"""Package stub."""
+"""Data source package."""
+
+from .base import AbstractSource
+
+# Internal import subtypes to trigger registration via __init_subclass__.
+from . import type as _register_subtypes  # pylint: disable=unused-import
+
+__all__ = ["AbstractSource"]
