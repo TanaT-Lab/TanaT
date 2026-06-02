67de8f77de5764c9edbed715bdd6c664d726f0aa
Arnaud Duvermy <arnaud.duvermy@inria.fr>
2026-06-02 15:58:51 +0200

    refactor: update module docstring and reorganize exports in sequence metric sub-package

--- src/tanat/metric/sequence/type/__init__.py
+++ src/tanat/metric/sequence/type/__init__.py
@@ -1,4 +1,27 @@
 #!/usr/bin/env python3
-"""
-Sequence metric type sub-package.
-"""
+"""Sequence metric subtypes."""
+
+from .chi2.metric import Chi2SequenceMetric, Chi2Settings
+from .dtw.metric import DTWSequenceMetric, DTWSettings
+from .edit.metric import EditSequenceMetric, EditSettings
+from .lcp.metric import LCPSequenceMetric, LCPSettings
+from .lcs.metric import LCSSequenceMetric, LCSSettings
+from .linear_pairwise.metric import LinearPairwiseSequenceMetric, LinearPairwiseSettings
+from .softdtw.metric import SoftDTWSequenceMetric, SoftDTWSettings
+
+__all__ = [
+    "Chi2SequenceMetric",
+    "Chi2Settings",
+    "DTWSequenceMetric",
+    "DTWSettings",
+    "EditSequenceMetric",
+    "EditSettings",
+    "LCPSequenceMetric",
+    "LCPSettings",
+    "LCSSequenceMetric",
+    "LCSSettings",
+    "LinearPairwiseSequenceMetric",
+    "LinearPairwiseSettings",
+    "SoftDTWSequenceMetric",
+    "SoftDTWSettings",
+]
