614a28c2b77c06ce681ccd59d723f4a723463e2a
Arnaud Duvermy <arnaud.duvermy@inria.fr>
2026-06-02 15:58:46 +0200

    refactor: reorganize imports in sequence metric sub-package for clarity

--- src/tanat/metric/sequence/__init__.py
+++ src/tanat/metric/sequence/__init__.py
@@ -4,31 +4,37 @@ Sequence metric sub-package.
 """
 
 from .base import SequenceMetric
-from .type.linear_pairwise.metric import (
+from .type import (
+    Chi2SequenceMetric,
+    Chi2Settings,
+    DTWSequenceMetric,
+    DTWSettings,
+    EditSequenceMetric,
+    EditSettings,
+    LCPSequenceMetric,
+    LCPSettings,
+    LCSSequenceMetric,
+    LCSSettings,
     LinearPairwiseSequenceMetric,
     LinearPairwiseSettings,
+    SoftDTWSequenceMetric,
+    SoftDTWSettings,
 )
-from .type.edit.metric import EditSequenceMetric, EditSettings
-from .type.lcs.metric import LCSSequenceMetric, LCSSettings
-from .type.lcp.metric import LCPSequenceMetric, LCPSettings
-from .type.dtw.metric import DTWSequenceMetric, DTWSettings
-from .type.softdtw.metric import SoftDTWSequenceMetric, SoftDTWSettings
-from .type.chi2.metric import Chi2SequenceMetric, Chi2Settings
 
 __all__ = [
     "SequenceMetric",
-    "LinearPairwiseSequenceMetric",
-    "LinearPairwiseSettings",
-    "EditSequenceMetric",
-    "EditSettings",
-    "LCSSequenceMetric",
-    "LCSSettings",
-    "LCPSequenceMetric",
-    "LCPSettings",
-    "DTWSequenceMetric",
-    "DTWSettings",
-    "SoftDTWSequenceMetric",
-    "SoftDTWSettings",
     "Chi2SequenceMetric",
     "Chi2Settings",
+    "DTWSequenceMetric",
+    "DTWSettings",
+    "EditSequenceMetric",
+    "EditSettings",
+    "LCPSequenceMetric",
+    "LCPSettings",
+    "LCSSequenceMetric",
+    "LCSSettings",
+    "LinearPairwiseSequenceMetric",
+    "LinearPairwiseSettings",
+    "SoftDTWSequenceMetric",
+    "SoftDTWSettings",
 ]
