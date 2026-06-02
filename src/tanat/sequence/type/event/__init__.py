555f61b990bb8a0879a5c53719daab48f22cb324
Arnaud Duvermy <arnaud.duvermy@inria.fr>
2026-06-02 16:42:57 +0200

    refactor: update package docstring and define event sequence subtypes

--- src/tanat/sequence/type/event/__init__.py
+++ src/tanat/sequence/type/event/__init__.py
@@ -1,2 +1,14 @@
 #!/usr/bin/env python3
-"""Package stub."""
+"""Event sequence subtypes."""
+
+from .entity import EventEntity
+from .pool import EventSequencePool
+from .sequence import EventSequence
+from .settings import EventSequenceSettings
+
+__all__ = [
+    "EventEntity",
+    "EventSequencePool",
+    "EventSequence",
+    "EventSequenceSettings",
+]
