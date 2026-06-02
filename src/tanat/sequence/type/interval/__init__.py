200a288cafca492f4404b51887287a5524eaba58
Arnaud Duvermy <arnaud.duvermy@inria.fr>
2026-06-02 16:42:51 +0200

    refactor: update package docstring and define interval sequence subtypes

--- src/tanat/sequence/type/interval/__init__.py
+++ src/tanat/sequence/type/interval/__init__.py
@@ -1,2 +1,14 @@
 #!/usr/bin/env python3
-"""Package stub."""
+"""Interval sequence subtypes."""
+
+from .entity import IntervalEntity
+from .pool import IntervalSequencePool
+from .sequence import IntervalSequence
+from .settings import IntervalSequenceSettings
+
+__all__ = [
+    "IntervalEntity",
+    "IntervalSequencePool",
+    "IntervalSequence",
+    "IntervalSequenceSettings",
+]
