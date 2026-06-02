0f74ad8c78daf7e573f8e3f4411703e558cf66bd
Arnaud Duvermy <arnaud.duvermy@inria.fr>
2026-06-02 15:59:18 +0200

    refactor: update package docstring and register AbstractSource subtypes

--- src/tanat/store/source/type/__init__.py
+++ src/tanat/store/source/type/__init__.py
@@ -1,2 +1,13 @@
 #!/usr/bin/env python3
-"""Package stub."""
+"""Register AbstractSource subtypes."""
+
+from .file import CsvSource, ParquetSource
+from .sql import SqlSource
+from .dataframe import DataFrameSource
+
+__all__ = [
+    "CsvSource",
+    "ParquetSource",
+    "SqlSource",
+    "DataFrameSource",
+]
